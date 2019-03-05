27d43426ec567ce177e2443bf574c314709321dc
ljrocha <ljrocha@users.noreply.github.com>
2019-03-04 20:39:57 -0800

    Initial Commit

---
+++ WordSearchGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordSearchGenerator
+//
+//  Created by Leandro Rocha on 3/4/19.
+//  Copyright © 2019 Leandro Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
