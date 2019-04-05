e4cc207f9b81796fa9e3acde765d1f6e9e3c980c
ljrocha <ljrocha@users.noreply.github.com>
2019-04-04 19:49:24 -0700

    Initial version of the word search puzzle

--- WordSearchGenerator/ViewController.swift
+++ WordSearchGenerator/ViewController.swift
@@ -12,7 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.url(forResource: "capitals", withExtension: "json")!
+        let contents = try! Data(contentsOf: path)
+        let words = try! JSONDecoder().decode([Word].self, from: contents)
+        
+        let wordSearch = WordSearch()
+        wordSearch.words = words
+        
+        let output = wordSearch.render()
+        let url = getDocumentsDirectory().appendingPathComponent("output.pdf")
+        try? output.write(to: url)
+        print(url)
+        
+        let ac = UIActivityViewController(activityItems: ["Checkout these word puzzles\n", url], applicationActivities: nil)
+        present(ac, animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 
 
