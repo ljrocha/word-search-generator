53335f3f02230b91657f92e7ca98ed9d0b266c54
ljrocha <ljrocha@users.noreply.github.com>
2019-06-23 21:33:17 -0700

    Create floating button. Floating button's action generates word search puzzle.

---
+++ WordSearchGenerator/WordSearchButton.swift
@@ -0,0 +1,35 @@
+//
+//  WordSearchButton.swift
+//  WordSearchGenerator
+//
+//  Created by Leandro Rocha on 6/23/19.
+//  Copyright © 2019 Leandro Rocha. All rights reserved.
+//
+
+import UIKit
+
+class WordSearchButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        buttonSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        buttonSetup()
+    }
+    
+    func buttonSetup() {
+        setTitle("WS", for: .normal)
+        contentEdgeInsets = UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
+        backgroundColor = .purple
+        layer.cornerRadius = 8
+        layer.shadowOpacity = 0.25
+        layer.shadowRadius = 5
+        layer.shadowOffset = CGSize(width: 0, height: 10)
+    }
+
+}
