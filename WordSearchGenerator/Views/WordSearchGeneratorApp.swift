6ef73dee442d9fea78998c7a3a9956a899670c9b
Leandro Rocha <ljrocha08@gmail.com>
2026-07-04 20:39:01 -0700

    Migrate app to scene lifecycle

---
+++ WordSearchGenerator/Views/WordSearchGeneratorApp.swift
@@ -0,0 +1,18 @@
+//
+//  WordSearchGeneratorApp.swift
+//  WordSearchGenerator
+//
+//  Created by Leandro Rocha on 7/4/26.
+//  Copyright © 2026 Leandro Rocha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WordSearchGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
